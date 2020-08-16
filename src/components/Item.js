3aaa76184f74a21ecd0b05384e30286789415c6c
phuong nguyen <thaiphuongit25@gmail.com>
2020-08-16 16:58:12 +0700

    custom component

--- src/components/Item.js
+++ src/components/Item.js
@@ -12,12 +12,14 @@ class Item extends Component {
         }
         this.name = React.createRef();
     }
+
     componentDidMount () {
-        let atr = localStorage.getItem("todo");
+        let atr = localStorage.getItem(this.props.itemName);
         if (atr) {
             this.setState({taskShows: JSON.parse(atr)});
         }
     }
+
     creatTask = () => {
         let text = this.name.current.value
         this.name.current.value = ''
@@ -27,7 +29,7 @@ class Item extends Component {
             let atr =  this.state.taskShows.concat([{name: text, id: idGenerator()}]);
             this.state.data = localStorage.getItem('settingTodo')
             if(atr.length <= this.state.data) {
-                localStorage.setItem("todo", JSON.stringify(atr))
+                localStorage.setItem(this.props.itemName, JSON.stringify(atr))
                 this.setState({
                     taskShows: atr
                 });
@@ -36,14 +38,17 @@ class Item extends Component {
             }
         }                
     }
+
     workDelete = value => {
         let todoArray = this.state.taskShows.filter(todo => todo.id !== value)
-        localStorage.setItem("todo", JSON.stringify(todoArray))
+        localStorage.setItem(this.props.itemName, JSON.stringify(todoArray))
         this.setState({taskShows: todoArray});
     }
+
     updateTask = e => {
         this.setState({task: e.target.value});
     }
+
     changeTask = e => {
         let todoTask = this.state.taskShows.find((emp) => {
             if (emp.id === e.target.id) {
@@ -54,17 +59,19 @@ class Item extends Component {
             task: todoTask.name
          });       
     }
+
     todoChange = (value) => {
         this.state.taskShows.map(e => {
             if(e.name === value) {
                 e.name = this.state.task
             }
         })
-
+        localStorage.setItem(this.props.itemName, JSON.stringify(this.state.taskShows))
         this.setState({ 
             taskShows: this.state.taskShows
         });
     }
+
     render() {
         return (
             <div className='kanbanBoard'>
@@ -86,14 +93,13 @@ class Item extends Component {
                                         {emp.name}
                                 </div>
                                 <div className='kanbanShow-deleteChange'>
-
                                     <span
                                         className="glyphicon glyphicon-trash kanbanDelete"
-                                        data-toggle="modal" data-target={`#${emp.id}`}
+                                        data-toggle="modal" data-target={`#d-${this.props.itemName}-${emp.id}`}
                                         type="button"
                                     >
                                     </span>
-                                    <div className="modal fade" id={`${emp.id}`} role="dialog">
+                                    <div className="modal fade" id={`d-${this.props.itemName}-${emp.id}`} role="dialog">
                                         <div className="modal-dialog">                              
                                             <div className="modal-content">
                                                 <div className="modal-body">
@@ -110,13 +116,13 @@ class Item extends Component {
 
                                     <span
                                         className="glyphicon glyphicon-pencil kanbanChange"
-                                        data-toggle="modal" data-target={`#change-${emp.id}`}
+                                        data-toggle="modal" data-target={`#c-${this.props.itemName}-${emp.id}`}
                                         onClick = {this.changeTask}
                                         id={emp.id}
                                         type="button"
                                     >
                                     </span>
-                                    <div className="modal fade" id={`change-${emp.id}`} role="dialog">
+                                    <div className="modal fade" id={`c-${this.props.itemName}-${emp.id}`} role="dialog">
                                         <div className="modal-dialog">                              
                                             <div className="modal-content">
                                                 <div className="modal-body">
